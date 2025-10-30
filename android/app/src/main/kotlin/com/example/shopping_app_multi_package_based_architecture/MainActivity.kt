9aa75b0524d08b09b85fc230926437c674bd5fba
Ruby Wai <62864134+rubywai@users.noreply.github.com>
2025-10-30 10:52:56 +0800

    fix: update flutter version

---
+++ android/app/src/main/kotlin/com/example/shopping_app_multi_package_based_architecture/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.shopping_app_multi_package_based_architecture
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/com/rubylearner/shopping_multipackage_architecture/shopping_app_multi_package_based_architecture/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.rubylearner.shopping_multipackage_architecture.shopping_app_multi_package_based_architecture
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
